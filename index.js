793c520bc457a366c7b0361a600e7c1f68f7b886
Toluwa Nafiu <nafiutc@rose-hulman.edu>
2018-06-05 10:13:54 -0400

    Submit button changes to input value!

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('testing')
+const button = document.querySelector('button')
+
+function changeHeader() {
+    const heading = document.querySelector('#second')
+    heading.textContent = "Lies, Harry's Spells Are Better"
+}
+
+button.addEventListener('click', changeHeader)
+
+const submit = document.querySelector("#submitForm")
+
+function changeWithSubmit() {
+    const secondHeading = document.querySelector('#second')
+    secondHeading.textContent = document.getElementById("myForm").elements.namedItem("headerinp").value;
+}
+
+submit.addEventListener('click', changeWithSubmit)
