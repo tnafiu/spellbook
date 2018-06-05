fb1b297a03bf36ed65c96fef0ce39596512a4ff7
Toluwa Nafiu <nafiutc@rose-hulman.edu>
2018-06-05 12:45:24 -0400

    The enter button works to submit as well and now it looks pretty!

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-const button = document.querySelector('button')
+const button = document.querySelector('button');
 
 function changeHeader() {
-    const heading = document.querySelector('#second')
-    heading.textContent = "Lies, Harry's Spells Are Better"
+    const heading = document.querySelector('#second');
+    heading.textContent = "Lies, Harry's Spells Are Better";
 }
 
-button.addEventListener('click', changeHeader)
+button.addEventListener('click', changeHeader);
 
-const submit = document.querySelector("#submitForm")
+const submit = document.querySelector("#submitForm");
 
 function changeWithSubmit() {
-    const secondHeading = document.querySelector('#second')
+    const secondHeading = document.querySelector('#second');
     secondHeading.textContent = document.getElementById("myForm").elements.namedItem("headerinp").value;
 }
 
-submit.addEventListener('click', changeWithSubmit)
+function enterPressed() {
+    if (event.keycode == 13) changeWithSubmit;
+}
+submit.addEventListener('click', changeWithSubmit);
+submit.addEventListener('keydown', enterPressed(event));
