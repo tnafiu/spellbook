051aa2af08cc3fda334791264181f569df5f5990
Toluwa Nafiu <nafiutc@rose-hulman.edu>
2018-06-07 09:23:31 -0400

    Add each entered spell into an array

--- index.js
+++ index.js
@@ -13,8 +13,10 @@ function changeHeader() {
 button.addEventListener('click', changeHeader);
 
 const app = {
+    // an array to hold the spells
+    spellArray: [],
     init: function() {
-        const form = document.querySelector('form')
+        const form = document.querySelector('form');
         form.addEventListener('submit', (ev) => {
             ev.preventDefault()
             this.changeWithSubmit(ev)
@@ -23,47 +25,58 @@ const app = {
 
     renderProperty: function(name, value) {
         // el stands for the element we're creating
-        const el = document.createElement('span')
-        el.classList.add(name)
-        el.textContent = value
-        el.setAttribute('title', value)
-        return el
+        const el = document.createElement('span');
+        el.classList.add(name);
+        el.textContent = value + " ";
+        el.setAttribute('title', value);
+        return el;
     },
 
     renderList: function(spell) {
         // ['name', 'level']
-        const properties = Object.keys(spell)
+        const properties = Object.keys(spell);
 
         // collects an array of <span> elements
         const childElements = properties.map((prop) => {
             return this.renderProperty(prop, spell[prop])
         })
 
-        const newList = document.createElement('li')
-        newList.classList.add('spell')
+        const newList = document.createElement('li');
+        newList.classList.add('spell');
 
         // append each <span> to the <li>
         childElements.forEach(function(el) {
             newList.appendChild(el)
-        })
+        });
 
-        return newList
+        return newList;
+    },
+
+    makeDeleteButton: function() {
+        deleteButton = document.createElement('input')
+        deleteButton.setAttribute("type", "submit")
+        deleteButton.setAttribute("value", "Delete")
+        return deleteButton;
     },
 
     changeWithSubmit: function(ev) {
-        const f = ev.target
+
+        const f = ev.target;
 
         const spell = {
             name: f.yourSpell.value,
             description: f.yourDescription.value,
         }
+        const val = Object.values(spell);
+        this.spellArray.push(val[0]);
+        console.log(this.spellArray);
 
-        const newList = this.renderList(spell)
+        const newList = this.renderList(spell);
 
-        const currentUl = document.querySelector('#spells')
-        currentUl.appendChild(item)
+        const currentUl = document.querySelector('#spells');
+        currentUl.appendChild(newList);
 
-        f.reset()
+        f.reset();
     },
 }
-app.init()
+app.init();
